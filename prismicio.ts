6f827ce2befdfc80b6186338bd6c629044137090
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2024-04-04 11:40:12 +0200

    revalidate 5 and name vercel

--- prismicio.ts
+++ prismicio.ts
@@ -36,9 +36,7 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
     fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
+    { next: { revalidate: 5 } },
     ...config,
   });
 
